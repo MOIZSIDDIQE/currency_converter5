a9930a166f794d79f452323987b0758f320f887e
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-04-19 00:55:23 +0500

    currency convert

---
+++ main.ts
@@ -0,0 +1,57 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let currency:any = {
+    USD: 1,
+    EUR: 0.94,
+    GBP: 0.80,
+    PKR: 278.25,
+    IND: 83.64,
+    AED: 3.67,
+}
+
+let answer : any= await inquirer.prompt(
+    [
+        {
+            type: "list",
+            name: "from",
+            message: "Enter from amount",
+            choices: [
+                "USD",
+                "EUR",
+                "GBP",
+                "PKR",
+                "IND",
+                "AED",
+            ]
+        }, {
+            name: "to",
+            type:"list",
+            message: "Enter to amount",
+            choices: [
+                "USD",
+                "EUR",
+                "GBP",
+                "PKR",
+                "IND",
+                "AED",
+            ]
+        },{
+            name: "amount",
+            type: "number",
+            message: "Enter convert amount"
+        }
+    ]
+)
+
+let fromAmount = currency[answer.from];
+let toAmount = currency[answer.to];
+let amount = answer.amount ; 
+let baseAmount = amount / fromAmount ;
+let convertAmount = baseAmount * toAmount ;
+
+
+console.log(answer);
+console.log(convertAmount);
+
